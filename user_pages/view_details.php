64dcb094c9146d10d3dad923417f093c89671fb4
Lungisa NDUNA <lnduna@e5r2p7.wethinkcode.co.za>
2018-10-31 16:02:57 +0200

    deleted research files and tried to fix the view details problem

--- user_pages/view_details.php
+++ user_pages/view_details.php
@@ -1,21 +1,17 @@
 <?PHP
 
-require_once("config/database.php");
+include_once("config/database.php");
 
-echo "<h1>THIS IS THE VIEW DETAILS PAGE</h1><br/>";
+echo "<h1><br/>THIS IS THE VIEW DETAILS PAGE</h1><br/>";
 
-/*$sql = 'SELECT FROM user * WHERE uid="'.$_POST['logged_uid'].'"';
+$sql = 'SELECT FROM user * WHERE uid="'.$_POST['logged_uid'].'"';
 $result = run_query($sql);
 
+$output = '<h2 style="text-align:center">User Profile</h2>';
 foreach ($result as $row){
-    $output = '<h2 style="text-align:center">User Profile</h2>
-                <div class="card">
-                    <h1>'.$row["uid"].'</h1>
-                    <p class="fullname">'.$row["fname"].' '.$row["lname"].'</p>
-                    <p>'.$row["email"].'</p>
-                    <p><button>Edit</button></p>
-                </div>';
-    echo "$output";
-    }*/
+        $output .= '<div class="card"><h1>'.$row["uid"].'</h1><p class="fullname">'.$row["fname"].' '.$row["lname"].'</p><p>'.$row["email"].'</p><p><button>Edit</button></p></div>';
+}
+
+echo $output;
 
 ?>
