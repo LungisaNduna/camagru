ae32df50495a6b8c8da13cc474b45647212dc1c4
Lungisa NDUNA <lnduna@e5r2p7.wethinkcode.co.za>
2018-11-07 16:39:03 +0200

    trying to fix the view details page, unlucky

--- user_pages/view_details.php
+++ user_pages/view_details.php
@@ -3,12 +3,17 @@
 include_once("config/database.php");
 
 echo "<h1><br/>THIS IS THE VIEW DETAILS PAGE</h1><br/>";
+$user = $_POST['logged_in'];
+echo "<p>working with $user</p><br/>";
+$sql = 'SELECT FROM user * WHERE uid="?"';
+$query = $db_conn->prepare($sql);
+$query->execute($user);
+echo "<p>ran '$sql' query</p>";
 
-$sql = 'SELECT FROM user * WHERE uid="'.$_POST['logged_uid'].'"';
-$result = run_query($sql);
+echo "<p>check</p>";
 
 $output = '<h2 style="text-align:center">User Profile</h2>';
-foreach ($result as $row){
+while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
         $output .= '<div class="card"><h1>'.$row["uid"].'</h1><p class="fullname">'.$row["fname"].' '.$row["lname"].'</p><p>'.$row["email"].'</p><p><button>Edit</button></p></div>';
 }
 
