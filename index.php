afda5fce1155d8fbef171ae771078858fba5bc67
Lungisa NDUNA <lnduna@e5r2p7.wethinkcode.co.za>
2018-11-22 16:20:22 +0200

    base for viewing posts has been established

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <?PHP
-session_start();
-if (!isset($_SESSION['logged_in'])){
-    $_SESSION['logged_in'] = false;
-    $_SESSION['logged_uid'] = "Guest";
-}
+    session_start();
+    if (!isset($_SESSION['logged_in'])){
+        $_SESSION['logged_in'] = false;
+        $_SESSION['logged_uid'] = "Guest";
+    }
+
+    function creatediv($postID, $posterUID, $imgData, $imgMess, $postLikeCount, $postCommentCount) {
+        $output =   '<div>';
+        $output .=  '<a href="view.php?post_num='.$postID.'"><img src="" alt="" height="300" width="400"></a>';
+        $output .=  '<p style="float: left;">posted by <em>'.$posterUID.'</em></p>';
+        $output .=  '<p style="float: right;">has '.$postLikeCount.' likes and '.$postCommentCount.' comments</p>';
+        $output .=  '<p>'.$imgMess.'</p>';
+        $output .=  '</div>';
+        echo $output;
+    }
+    //get the first 10 images from the database store in a variable
+
+    //show 5 images at a time
+
+    //create pages that allow the user to view the next 5 and so on
 ?>
 
 <html>
@@ -15,7 +30,6 @@ if (!isset($_SESSION['logged_in'])){
         <?PHP include("format/navbar.php");
             if ($_SESSION['logged_in']){ echo "User logged in\n"; } else { echo "User not logged in\n"; }
             echo "as ".$_SESSION['user_id']."\n";
-
             include("format/footer.php");
         ?>
     </body>
--- view.php
+++ view.php
@@ -1,17 +1,61 @@
 <?php
-    include("format/navbar.php");
+    
     include_once("config/database.php");
     echo "<h1>THIS IS WHERE ONE CAN VIEW POSTS OR A SINGLE POST</h1>";
-    
-    //get the first 10 images from the database store in a variable
 
-    //show 5 images at a time
+    //simply show a single post and it's comments.
 
-    //create pages that allow the user to view the next 5 and so on
+    $postID = $_GET["post_num"];
+
+    //unserialize the comments and place them into divs to output them
+    //this outputs each comment as it is finished so as not to wait for all the comments to be processed before display
+    function    list_comments($serialisedComments) {
+        $comments = unserialize($serialisedComments);
+        foreach ($comments as $comment) {
+            $output = '<div class="commentDiv">';
+            $output .= '<h5 class="commentor">'.$comment["uid"].'</h5>';
+            $output .= '<p class="comment">'.$comment["comment"].'</p>';
+            $output .= '</div>';
+            echo $output;
+        }
+    }
+
+    //grab everything about the post from the data base
+
+    if ($_SESSION['logged_in']) {
+        $firstthing = '<form action="comment" method="post">';
+        $firstthing .= '<input type="text" name="new_comment" placeholder="Want to comment simply add it">';
+        $firstthing .= '<input type="submit" value="Comment">';
+        $firstthing .= '</form>';
+    } else { 
+        $firstthing = "<p id='logInNotice'>";
+        $firstthing .= "You need to sign in to leave a comment. ";            
+        $firstthing .= "You can sign in or up using the buttons in the top right corner";
+        $firstthing .= "</p>"; 
+    }
 ?>
 
+<html>
+    <head>
+
+    </head>
+    <body>
+        <?PHP include("format/navbar.php"); ?>
+        <?PHP include("format/footer.php"); ?>
+        <?PHP
+            
+            //place picture
+
+            echo $firstthing;
+
+            list_comments($raw_comments);
+        ?>
+
+        
 
 
-<?PHP
-    include("format/footer.php");
-?>
+        
+    </body>
+</html>
+
+
